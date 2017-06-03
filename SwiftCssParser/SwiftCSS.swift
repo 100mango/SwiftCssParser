f4de3b8f825d105ef693027bc530aeab58396e77
100mango <100mango@gmail.com>
2017-06-03 19:48:05 +0800

    Add SwiftCSS

---
+++ SwiftCssParser/SwiftCSS.swift
@@ -0,0 +1,75 @@
+//
+//  SwiftCSS.swift
+//  SwiftCssParser
+//
+//  Created by Mango on 2017/6/3.
+//  Copyright © 2017年 Mango. All rights reserved.
+//
+
+import UIKit
+
+public class SwiftCSS {
+    
+    private let parsedCss: [String:[String:Any]]
+    
+    init(CssFileURL: URL) {
+        let content = try! String(contentsOf: CssFileURL, encoding: .utf8)
+        let lexer = CssLexer(input: content)
+        let parser = CssParser(lexer: lexer)
+        parsedCss = parser.outputDic
+    }
+    
+    
+    func int(selector: String, key: String) -> Int {
+        
+        return Int(double(selector: selector, key: key))
+    }
+    
+    func double(selector: String, key: String) -> Double {
+        return value(selector: selector, key: key) ?? 0
+    }
+    
+    func string(selector: String, key: String) -> String {
+        return value(selector: selector, key: key) ?? ""
+    }
+    
+    func color(selector: String, key: String) -> UIColor {
+        
+        if let rgb:(Double,Double,Double,Double) = value(selector: selector, key: key) {
+            return UIColor(red: CGFloat(rgb.0/255), green: CGFloat(rgb.1/255), blue: CGFloat(rgb.2/255), alpha: CGFloat(rgb.3))
+        } else {
+            return UIColor(string(selector: selector, key: key))
+        }
+        
+    }
+    
+    func font(selector: String, key: String, fontSize: CGFloat = 14) -> UIFont {
+        
+        if let name: String = value(selector: selector, key: key) {
+            
+            return UIFont(name: name, size: fontSize) ?? UIFont.systemFont(ofSize: fontSize)
+            
+        } else if let dic: [String:Any] = value(selector: selector, key: key) {
+            
+            guard let name = dic["name"] as? String ,let size = dic["size"] as? Double else {
+                return UIFont.systemFont(ofSize: fontSize)
+            }
+            return UIFont(name: name, size: CGFloat(size)) ?? UIFont.systemFont(ofSize: fontSize)
+            
+        } else {
+            return UIFont.systemFont(ofSize: fontSize)
+        }
+    }
+    
+    private func value<T>(selector: String, key: String) -> T? {
+        guard let dic = parsedCss[selector] else {
+            return nil
+        }
+        guard let value = dic[key] as? T else {
+            return nil
+        }
+        
+        return value
+    }
+    
+}
